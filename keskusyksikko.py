904632192d5f3f9a8454564b8eb14a003ca986b9
Akseli Konttas <akseli.konttas@tecnotree.com>
2026-03-20 15:40:14 +0200

    Improved documentation

--- keskusyksikko.py
+++ keskusyksikko.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 """
-Program to take Artnet commands and pass them forward to a stepper motor.
-Tuomas Valtonen / Akseli Konttas 2026
+Program to control a stepper motor with ArtNet signal.
+- Listens to ArtNet
+- Processes the signals
+- Controls the motor according to the signals
+
+Requires python_artnet and pigpio packages.
+In addition, pigpio daemon must be running on the system. More info: https://abyz.me.uk/rpi/pigpio/download.html
+
+Akseli Konttas / Tuomas Valtonen 2026
 """
+import sys
 import time
-import pigpio
-from python_artnet import python_artnet
 import logging
 from logging.handlers import RotatingFileHandler
+
+import pigpio
+from python_artnet import python_artnet
+
 
 
 ## ============================
 
 ## ARTNET PARAMETERS
 
-DMX_CHANNEL_DIRECTION   = 1     # Motor direction channel (low=clockwise, high=counter-clockwise)
-DMX_CHANNEL_SPEED       = 2     # Motor speed channel (0=off, 255=max speed)
+DMX_CHANNEL_DIRECTION   = 1     # DMX channel controlling motor direction   (low=clockwise, high=counter-clockwise)
+DMX_CHANNEL_SPEED       = 2     # DMX channel controlling motor speed       (0=off, 255=max speed)
 
 ARTNET_BIND_IP  = "0.0.0.0"
 ARTNET_UNIVERSE = 15
@@ -31,6 +41,8 @@ SPEED_PIN   = 13    # Motor rotation speed (PWM). Higher PWM frequency -> faster
 # Maximum PWM frequency (MAX_FREQ -> max speed). Artnet value 255 (=max value) corresponds to this frequency. Modify this accordingly if needed
 MAX_FREQ    = 12_800  # Hz
 
+
+## ============================
 
 # Set up logging
 rfh_debug = RotatingFileHandler("keskusyksikko_debug.log", encoding="utf-8", maxBytes=10*1024*1024, backupCount=3)  # Log file logging all events (including debug)
@@ -74,7 +86,8 @@ def init_gpio(dir_pin, speed_pin, enable_pin) -> pigpio.pi:
     # Enable the motor (enable_pin to HIGH)
     pi.write(enable_pin, 1)
 
-    logging.info(f"PIGPIO ready. Direction pin {dir_pin}, step pin {speed_pin}, enable pin {enable_pin}. Set enable pin to HIGH.")
+    logging.info(f"PIGPIO ready.")
+    logging.debug(f"Direction pin {dir_pin}, step pin {speed_pin}, enable pin {enable_pin}. Set enable pin to HIGH.")
     return pi
 
 
@@ -83,24 +96,31 @@ def dmx_value_to_pwm_frequency(dmx_val, max_pwm_freq=MAX_FREQ, max_value=255, st
     Maps DMX value in range [0, max_value] to PWM frequency in range [0, max_pwm_freq]:
         dmx_val <= stop_treshold  ---> returns 0
         dmx_val == max_value      ---> returns max_pwm_freq.
+    @param dmx_val:         The raw integer value from DMX packet
+    @param max_pwm_freq:    Maximum PWM frequency. This should correspond to the maximum desired rotation speed.
+    @param max_value:       Maximum possible value from DMX packet; this value should also correspond to the maximum rotation speed
+    @param stop_treshold:   DMX value treshold to start the rotation; under this value (inclusive) stops the motor
+    @returns:               The PWM frequency (in range [0, max_pwm_freq]) corresponding to the parameters (int)
     '''
     freq = int(max_pwm_freq * (dmx_val-stop_treshold)/(max_value-stop_treshold))
     freq = max(0, freq)     # Set values under zero to zero
+    freq = min(freq, max_pwm_freq) # Cap to max frequency (this should only happen if the given value is higher than max_value)
     return freq
 
 
-def set_motor_speed(pi: pigpio.pi, speed: int, clockwise: bool=True, dir_pin=DIR_PIN, speed_pin=SPEED_PIN, max_pwm_freq=MAX_FREQ):
+def set_motor_speed(pi: pigpio.pi, speed: int, clockwise: bool=True, dir_pin=DIR_PIN, speed_pin=SPEED_PIN, max_pwm_freq=MAX_FREQ) -> None:
     '''
     Set the motor to rotate with a certain speed and direction.
-    @param speed: Int in range [0,255], where 255 is maximum speed and 0 stops the motor.
-    @param clockwise: True for clockwise rotation, False for counter-clockwise
-    @param dir_pin: GPIO pin for motor direction
-    @para
+    @param speed:           Int in range [0,255], where 255 is maximum speed and 0 stops the motor.
+    @param clockwise:       True for clockwise rotation, False for counter-clockwise
+    @param dir_pin:         GPIO pin for motor direction
+    @param speed_pin:       PWM GPIO pin for rotation speed
+    @param max_pwm_freq:    PWM frequency corresponding to the maximum rotation speed
     '''
     stop_treshold   = 10                        # Speed this or less -> considered as 0 -> stop the motor
     pwm_duty        = 500_000                   # 0 off, 1M fully on -> 500K is 50/50
-    freq            = dmx_value_to_pwm_frequency(speed, max_pwm_freq, stop_treshold=stop_treshold)
     direction       = 1 if clockwise else 0     # Direction pin high (1) if clockwise, low (0) if counter-clockwise
+    freq            = dmx_value_to_pwm_frequency(speed, max_pwm_freq, stop_treshold=stop_treshold)  # Map the DMX value to an actual PWM frequency
 
     # Set the direction as needed
     pi.write(dir_pin, direction)
@@ -114,33 +134,31 @@ def set_motor_speed(pi: pigpio.pi, speed: int, clockwise: bool=True, dir_pin=DIR
 def main():
     logging.info("Starting the application")
     print("Keskusyksikko v1.0.0 käynnistyy...")
+    return_code = 0
 
     try:
         # Initialize ArtNet
-        artnet  = init_artnet(ARTNET_BIND_IP, debug=False)
+        artnet = init_artnet(ARTNET_BIND_IP)
 
         # Initialize gpio
-        pi      = init_gpio(DIR_PIN, SPEED_PIN, ENABLE_PIN)
+        pi = init_gpio(DIR_PIN, SPEED_PIN, ENABLE_PIN)
 
+        print("Keskusyksikkö toiminnassa.")
 
         # Loop until exit :)
         while True:
             time.sleep(0.01)
             
-            # Fetch last ArtNet package
+            # Fetch last ArtNet package, and skip if no buffer found
             artnet_buffer = artnet.readBuffer()
-
-            # Did we get a buffer?
             if artnet_buffer is None: continue
-            
-            artnet_packet = artnet_buffer[ARTNET_UNIVERSE]
 
-            # Did we get a package?
+            # Fetch last ArtNet packet, and skip if no packet found
+            artnet_packet = artnet_buffer[ARTNET_UNIVERSE]
             if artnet_packet is None: continue
 
+            # Fetch the DMX data from the ArtNet packet, and skip if no data
             dmx_packet = artnet_packet.data
-
-            # Is there any data?
             if dmx_packet is None: continue
 
             # Direction and speed from the dmx package
@@ -162,14 +180,20 @@ def main():
         logging.exception('Got an exception')
         logging.critical("Aborting execution.")
         print("Keskusyksikkö kaatui :(")
+        return_code = 1
 
     finally:
+        logging.debug("Closing ArtNet connection.")
         try:    artnet.close()
-        except: logging.exception('Got an exception when closing Artnet')
-        try:    set_motor_speed(pi, 0)
-        except: logging.exception('Got an exception when stopping the motor')
+        except: logging.exception('Got an exception when closing Artnet'); return_code=1
+        logging.debug("Stopping motor")
+        try:    set_motor_speed(pi, 0)  # (Try to) stop the motor when exiting the script
+        except: logging.exception('Got an exception when stopping the motor'); return_code=1
         logging.info("Exiting the application.")
+
+    return return_code
 
 
 if __name__ == "__main__":
-    main()
+    return_code = main()
+    sys.exit(return_code)
